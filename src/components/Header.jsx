4d6dfe385a69d8fc1e2ef64d8887df0bb1433ccd
david-l6 <github.decimal773@passmail.net>
2024-06-19 16:20:02 +0200

    test 7

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
       <nav>
         <ul>
           <NavLink
-            to="/kasa/"
+            to="/home"
             className={({ isActive }) => (isActive ? "underline" : "")}
           >
             <li>Accueil</li>
